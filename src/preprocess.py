8c111d35cb2965ad518677ef66aad5c247d53688
Anjana Raj <anjanaraj297@gmail.com>
2026-01-09 12:41:59 +0530

    Create preprocess.py
    Add preprocessing module

---
+++ src/preprocess.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def basic_preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Minimal preprocessing placeholder.
+    Extend when real data are available.
+    """
+    return df.copy()
